ce132d2bef2391983e7b93bba16f27d83efaa79f
DixLan <93582478+DixLan@users.noreply.github.com>
2023-10-16 03:13:26 +0200

    Update of the repository link

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <div class="container">
     <h1>Bienvenue sur notre site de sensibilisation aux liens</h1>
     <p>Ce site est conçu dans un but purement éducatif. Aucune donnée n'est stockée.</p>
-    <p>Vous pouvez consulter le code source de ce site sur <a href="https://github.com/votre-username/votre-repository">GitHub</a>.</p> <!-- Ajout du lien GitHub -->
+    <p>Vous pouvez consulter le code source de ce site sur <a href="https://github.com/DixLan/DixLan-DATA" target="_blank">GitHub</a>.</p> <!-- Ajout du lien GitHub -->
     <p>En cliquant sur le bouton ci-dessous, vous acceptez que nous utilisions certaines informations vous concernant afin de vous les afficher. Nous ne stockons aucune de ces informations !</p> <!-- Correction de fautes de frappe -->
 
     <!-- Lien vers la page où l'expérience démarre -->
